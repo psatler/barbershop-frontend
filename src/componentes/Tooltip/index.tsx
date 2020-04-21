2f6c73fc060518c3611ebf4b50a28f7a38e2e729
Pablo Satler <pablosatler@gmail.com>
2020-04-21 15:52:30 -0300

    feat: create tooltip component

---
+++ src/componentes/Tooltip/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+interface TooltipProps {
+  title: string;
+  className?: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({ children, className, title }) => {
+  // children here is usually a svg icon
+  return (
+    <Container className={className}>
+      {children}
+      <span> {title} </span>
+    </Container>
+  );
+};
+
+export default Tooltip;

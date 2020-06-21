2258d6fdceac1e07f80b650a2db28450abe759fa
Pablo Satler <pablosatler@gmail.com>
2020-06-21 13:33:22 -0300

    test: install testing-library/react-hooks

---
+++ src/__tests__/hooks/auth.spec.tsx
@@ -0,0 +1,18 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useAuth, AuthProvider } from '../../hooks/auth';
+
+describe('Auth hook', () => {
+  it('should be able to sign in', () => {
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    // console.log(result.current);
+    result.current.signIn({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(result.current.user.email).toEqual('johndoe@example.com');
+  });
+});
